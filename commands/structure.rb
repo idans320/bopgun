97372d2a44025299cbb5dfc2331c2eacf4398ea5
Idan Shani <idan@trustedpartners.com>
2024-03-06 16:59:35 +0200

    move exec to bin for easy add to path setting

--- commands/structure.rb
+++ commands/structure.rb
@@ -6,13 +6,13 @@ module StructureCommand
   include Parser
   def parse(name, project_name)
     file_name = "structures/#{name}"
-    throw IOError("#{name} is not a valid structure") unless File.exist?(file_name)
+    throw error("#{name} is not a valid structure") unless File.exist?(file_name)
     parse_structure_token(tokenize_structure((p File.read(file_name) % project_name)))
   end
 
   def create(name, project_name, pwd)
     project_dir =  File.join pwd, project_name
-    throw IOError('Project already exists') if Dir.exist? project_dir
+    throw error('Project already exists') if Dir.exist? project_dir
     Dir.mkdir project_dir
     parsed_paths = parse(name, project_name)
     parsed_paths.each do |path|
